9defb8df1c502886051ac0901fef417ee86db1d8
Peter Tuan <xpetahzx@gmail.com>
2018-06-18 19:06:44 -0400

    Test

--- spark/spark_stream_consumer.py
+++ spark/spark_stream_consumer.py
@@ -46,17 +46,17 @@ class AverageSpreadConsumer(SparkStreamConsumer):
 
         average_spread_dstream = spread_sum_count_dstream.mapValues(lambda x: x[0] / x[1])
 
-        def store_to_redis(rdd):
-            r = redis.StrictRedis(host='redis-ec-cluster.v7ufhi.clustercfg.use1.cache.amazonaws.com', port=6379,
-                                  db=0)
+        r = redis.StrictRedis(host='redis-ec-cluster.v7ufhi.clustercfg.use1.cache.amazonaws.com', port=6379,
+                              db=0)
 
+        def store_to_redis(rdd):
             def send_message(partition):
                 partition.foreach(lambda msg: r.set('a', 'test'))
 
             rdd.foreachPartition(send_message)
 
-        average_spread_dstream.foreachRDD(store_to_redis)
+        average_spread_dstream.foreach(lambda x: r.set('a', 'test'))
 
-        average_spread_dstream.pprint()
+        # average_spread_dstream.pprint()
 
         super().consume()
