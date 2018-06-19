9c41ae6be80554d842be36470dad82701e1db043
Peter Tuan <xpetahzx@gmail.com>
2018-06-19 16:31:56 -0400

    test; git push bitbucket master
    ;

--- spark/spark_stream_consumer.py
+++ spark/spark_stream_consumer.py
@@ -54,3 +54,18 @@ class AverageSpreadConsumer(SparkStreamConsumer):
         # average_spread_dstream.pprint()
 
         super().consume()
+
+
+if __name__ == '__main__':
+    # Kraken asset pairs for BTC, ETH, and LTC to USD prices
+    asset_pairs = ['XXBTZUSD', 'XETHZUSD', 'XLTCZUSD', 'BCHXBT']
+    methods = ['Spread']
+    topics = []
+
+    for asset_pair in asset_pairs:
+        for method in methods:
+            topic_name = '{}_{}'.format(asset_pair, method)
+            topics.append(topic_name)
+
+    consumer = AverageSpreadConsumer()
+    consumer.consume(topics)
