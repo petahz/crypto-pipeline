70a3f9a182bfb641b243300c63845835c15f1acc
Peter Tuan <xpetahzx@gmail.com>
2018-06-22 14:20:07 -0400

    Test

--- kafka/consumers/s3_consumer.py
+++ kafka/consumers/s3_consumer.py
@@ -22,8 +22,7 @@ class S3Consumer:
 
         for message in consumer:
             if message is not None:
-                print ('message: ', message)
-                asset_pair = message.key
+                asset_pair = message.partition_key
                 content = json.loads(message.value.decode())
                 body_content.append(content)
                 timestamp = content[0]
@@ -36,7 +35,7 @@ class S3Consumer:
                     s3.put_object(Body=json.dumps(body_content), Bucket=self.bucket_name, Key=key)
                     body_content = []
 
-                #print(message.offset, message.value)
+                print(message.offset, message.partition_key, message.value)
 
 
 if __name__ == '__main__':
