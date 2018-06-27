9e4d2f2b59f032c220cbac2b462f0fc24f4f895b
Peter Tuan <xpetahzx@gmail.com>
2018-06-27 17:20:43 -0400

    Set websocket connection

--- flask/static/src/containers/HomeContainer/index.js
+++ flask/static/src/containers/HomeContainer/index.js
@@ -4,7 +4,7 @@ import openSocket from 'socket.io-client';
 /* components */
 import { Home } from '../../components/Home';
 
-const socket = openSocket('http://0.0.0.0:5000');
+const socket = openSocket('http://localhost:5000');
 socket.on('liveData', (data) => {
     console.log('received new data: ', data);
 });
