4f4f76b2de2cb7622c9b3593d6e14b49bf28f6de
Peter Tuan <xpetahzx@gmail.com>
2018-06-27 18:06:04 -0400

    Set websocket connection

--- flask/static/src/components/Home/index.js
+++ flask/static/src/components/Home/index.js
@@ -24,33 +24,33 @@ export class Home extends React.Component {
                 <TableBody displayRowCheckbox={false}>
                     <TableRow>
                         <TableRowColumn>Bitcoin</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XXBTZUSD'].bid}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XXBTZUSD'].ask}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XXBTZUSD'].spread}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XXBTZUSD'] && this.props.liveData['XXBTZUSD'].bid}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XXBTZUSD'] && this.props.liveData['XXBTZUSD'].ask}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XXBTZUSD'] && this.props.liveData['XXBTZUSD'].spread}</TableRowColumn>
                     </TableRow>
                     <TableRow>
                         <TableRowColumn>Ethereum</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XETHZUSD'].bid}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XETHZUSD'].ask}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XETHZUSD'].spread}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XETHZUSD'] && this.props.liveData['XETHZUSD'].bid}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XETHZUSD'] && this.props.liveData['XETHZUSD'].ask}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XETHZUSD'] && this.props.liveData['XETHZUSD'].spread}</TableRowColumn>
                     </TableRow>
                     <TableRow>
                         <TableRowColumn>EOS</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['EOSUSD'].bid}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['EOSUSD'].ask}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['EOSUSD'].spread}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['EOSUSD'] && this.props.liveData['EOSUSD'].bid}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['EOSUSD'] && this.props.liveData['EOSUSD'].ask}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['EOSUSD'] && this.props.liveData['EOSUSD'].spread}</TableRowColumn>
                     </TableRow>
                     <TableRow>
                         <TableRowColumn>Litecoin</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XLTCZUSD'].bid}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XLTCZUSD'].ask}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['XLTCZUSD'].spread}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XLTCZUSD'] && this.props.liveData['XLTCZUSD'].bid}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XLTCZUSD'] && this.props.liveData['XLTCZUSD'].ask}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['XLTCZUSD'] && this.props.liveData['XLTCZUSD'].spread}</TableRowColumn>
                     </TableRow>
                     <TableRow>
                         <TableRowColumn>Bitcoin Cash</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['BCHUSD'].bid}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['BCHUSD'].ask}</TableRowColumn>
-                        <TableRowColumn>{this.props.liveData['BCHUSD'].spread}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['BCHUSD'] && this.props.liveData['BCHUSD'].bid}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['BCHUSD'] && this.props.liveData['BCHUSD'].ask}</TableRowColumn>
+                        <TableRowColumn>{this.props.liveData['BCHUSD'] && this.props.liveData['BCHUSD'].spread}</TableRowColumn>
                     </TableRow>
                 </TableBody>
             </Table>
