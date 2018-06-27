6cf236be3287da58b4010a5462e660a760a36949
Peter Tuan <xpetahzx@gmail.com>
2018-06-27 16:23:20 -0400

    Set websocket connection

--- flask/static/src/containers/HomeContainer/index.js
+++ flask/static/src/containers/HomeContainer/index.js
@@ -4,7 +4,7 @@ import openSocket from 'socket.io-client';
 /* components */
 import { Home } from '../../components/Home';
 
-const socket = openSocket('http://http://ec2-54-156-187-154.compute-1.amazonaws.com/:5000');
+const socket = openSocket('http://ec2-54-156-187-154.compute-1.amazonaws.com:5000');
 socket.on('liveData', (data) => {
     console.log('received new data: ', data);
 });
