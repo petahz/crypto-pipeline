d3448f35c6b8217b91ab2aab098d4af40aa3d24c
Peter Tuan <xpetahzx@gmail.com>
2018-06-27 23:06:51 -0400

    Set websocket connection

--- flask/manage.py
+++ flask/manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from flask_socketio import SocketIO
 from application.app import app, db, socketio
@@ -8,6 +8,7 @@ manager = Manager(app)
 
 # migrations
 manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
 
 
 @manager.command
