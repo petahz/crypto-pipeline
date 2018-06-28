6a8179d1f8a891139dc6e32080eae4b33b0c88ee
Peter Tuan <xpetahzx@gmail.com>
2018-06-28 10:58:05 -0400

    Set websocket connection

--- spark_stream.sh
+++ spark_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${SPARK_HOME}/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 --py-files spark/spark_stream_consumer.py spark_main.py
+${SPARK_HOME}/bin/spark-submit --deploy-mode cluster --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 --py-files spark/spark_stream_consumer.py spark_main.py
