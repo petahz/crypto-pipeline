02b98160722e2cd468ff5b40b10b1d64b083080b
Peter Tuan <xpetahzx@gmail.com>
2018-06-28 15:35:49 -0400

    Set websocket connection

--- spark/spark_stream_consumer.py
+++ spark/spark_stream_consumer.py
@@ -73,3 +73,25 @@ class SparkStreamConsumer:
         average_spread_dstream = spread_sum_count_dstream.mapValues(lambda x: x[0] / x[1])
 
         average_spread_dstream.foreachRDD(lambda rdd: rdd.foreachPartition(set_redis_avg_spread))
+
+if __name__ == '__main__':
+    # Kraken asset pairs for BTC, ETH, and LTC to USD prices
+    asset_pairs = ['XXBTZUSD', 'XETHZUSD', 'EOSUSD', 'XLTCZUSD', 'BCHXBT']
+    intervals = [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]
+    spread_topics = []
+    ohlc_topics = []
+
+    for asset_pair in asset_pairs:
+        # Spread
+        topic_name = '{}_{}'.format('Kraken', 'Spread')
+        spread_topics.append(topic_name)
+
+        # for interval in intervals:
+            # topic_name = '{}_{}_{}'.format('Kraken', 'OHLC', interval)
+            # consumer = FinancialMetricStreamConsumer(interval)
+            # consumer.consume(ohlc_topics)
+
+    consumer = SparkStreamConsumer()
+    consumer.consume_spreads(spread_topics)
+    consumer.start_stream()
+
--- spark_stream.sh
+++ spark_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${SPARK_HOME}/bin/spark-submit --master spark://ec2-34-235-103-8.compute-1.amazonaws.com:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 --py-files spark/spark_stream_consumer.py spark_main.py
+${SPARK_HOME}/bin/spark-submit --master spark://ec2-34-235-103-8.compute-1.amazonaws.com:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 spark/spark_stream_consumer.py
