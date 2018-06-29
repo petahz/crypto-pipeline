f5ce4ea8bae76f3cd9c1406642743bc0f29d5e10
Peter Tuan <xpetahzx@gmail.com>
2018-06-29 13:45:42 -0400

    Set websocket connection

--- flask/static/src/components/CoinRow/index.js
+++ flask/static/src/components/CoinRow/index.js
@@ -32,11 +32,11 @@ export class CoinRow extends React.Component {
         }
     }
 
-    shouldComponentUpdate(nextProps) {
-        const differentBid = this.props.values.bid !== nextProps.values.bid;
-        const differentAsk = this.props.values.ask !== nextProps.values.ask;
-        return differentBid || differentAsk;
-    }
+    // shouldComponentUpdate(nextProps) {
+    //     const differentBid = this.props.values.bid !== nextProps.values.bid;
+    //     const differentAsk = this.props.values.ask !== nextProps.values.ask;
+    //     return differentBid || differentAsk;
+    // }
 
     render() {
         return (
