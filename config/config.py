755b577d76b86cc07b4d48e0954803adb63e70f7
Peter Tuan <xpetahzx@gmail.com>
2018-07-02 21:46:24 -0400

    The right kafka nodes

--- config/config.py
+++ config/config.py
@@ -1,2 +1,2 @@
-KAFKA_NODES = ['ec2-52-44-121-53.compute-1.amazonaws.com:9092', 'ec2-52-22-234-28.compute-1.amazonaws.com:9092',
-                 'ec2-52-45-23-147.compute-1.amazonaws.com:9092', 'ec2-18-207-65-150.compute-1.amazonaws.com:9092']
+KAFKA_NODES = ['ec2-34-233-220-40.compute-1.amazonaws.com:9092', 'ec2-35-170-216-253.compute-1.amazonaws.com:9092',
+               'ec2-34-235-103-8.compute-1.amazonaws.com:9092', 'ec2-52-207-23-179.compute-1.amazonaws.com:9092']
